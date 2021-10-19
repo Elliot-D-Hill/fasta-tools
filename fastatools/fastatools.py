e801df6e9796bc05c1af48fbf916def97a1c2580
stillahill <elliot.douglas.hill@gmail.com>
2021-10-19 16:38:08 -0400

    Add FastaDatasetMaker class

--- fastatools/fastatools.py
+++ fastatools/fastatools.py
@@ -2,64 +2,54 @@ import pandas as pd
 import json
 from pathlib import Path
 from Bio import Entrez
-from abc import ABC, abstractmethod
+from datasetmaker.datasetmaker import DatasetMaker
+from abc import abstractmethod
 
 
-class FastaDatasetMaker(ABC):
-    def __init__(self, raw_path, cleaned_path, fasta_path, processed_path, email, ncbi_api_key):
-        self.raw_path = raw_path
-        self.cleaned_path = cleaned_path
-        self.fasta_path = fasta_path
-        self.processed_path = processed_path
-        self.email = email
-        self.ncbi_api_key = ncbi_api_key
+class FastaDatasetMaker(DatasetMaker):
+
+    def __init__(self, raw_path, processed_path):
+        super().__init__(raw_path, processed_path)
         self.column_names = ['pdb_code', 'chain',
                              'chain_type', 'description', 'sequence']
-        self.make_cleaned_dataframe()
-        self.df = read_dataframe(self.cleaned_path)
-        self.pdb_codes = self.get_pdb_codes()
-
-    @abstractmethod
-    def filter_dataframe(self, df):
-        pass
-
-    @abstractmethod
-    def format_dataframe(self, df):
-        pass
 
     @abstractmethod
     def assign_chain_type(self, df):
         pass
 
-    def make_cleaned_dataframe(self):
-        (read_dataframe(self.raw_path)
-            .pipe(self.format_dataframe)
-            .pipe(self.filter_dataframe)
-            .reset_index(drop=True)
-            .to_csv(self.cleaned_path, index=False)
-         )
+    def format_dataframe(self, df):
+        return self.process_header(df)
 
-    def get_pdb_codes(self):
-        return self.df['pdb_code'].str[0:4].unique()
+    def filter_dataframe(self, df):
+        return df
 
-    def make_dataset(self):
-        write_fasta_from_ncbi_query(
-            self.pdb_codes, self.fasta_path, self.email, self.ncbi_api_key)
-        (make_fasta_dataframe(self.fasta_path)
-            .pipe(process_header)
-            .pipe(self.assign_chain_type)
-            .pipe(collapse_rows, 'chain')
-            .pipe(organize_fasta_dataframe, self.column_names)
-            .to_csv(self.processed_path, index=False)
-         )
+    def transform_dataframe(self, df):
+        return (df
+                .pipe(self.assign_chain_type)
+                .pipe(self.collapse_rows, 'chain')
+                )
 
+    def organize_dataframe(self, df):
+        return (df[self.column_names]
+                .sort_values(['pdb_code', 'chain'], ignore_index=True))
 
-def read_dataframe(filepath):
-    extension = str(filepath).split('.')[-1]
-    if extension == 'tsv':
-        return pd.read_csv(filepath, sep='\t')
-    elif extension == 'csv':
-        return pd.read_csv(filepath, sep=',')
+    def process_header(self, df):
+        header = df['header']
+        header = header.str.split('|')
+        df['pdb_code'] = header.str[1]
+        description = header.str[2]
+        df['chain'] = description.str[0]
+        df['description'] = description.str.split(', ').str[-1]
+        return df[['pdb_code', 'chain', 'description', 'sequence']]
+
+    def sort_then_join(self, lst):
+        return ','.join(sorted(lst))
+
+    # collapse rows that differ only by one column
+    def collapse_rows(self, df, column_to_collapse):
+        columns = [n for n in df.columns if n != column_to_collapse]
+        df = df.groupby(columns)[column_to_collapse].apply(self.sort_then_join)
+        return df.reset_index()
 
 
 def read_text_file(filepath):
@@ -80,8 +70,8 @@ def process_fasta(file_text):
 
 
 # returns a dataframe of sequences and headers from a FASTA file
-def make_fasta_dataframe(filepath):
-    fasta = read_text_file(filepath)
+def make_dataframe_from_fasta(fasta_path):
+    fasta = read_text_file(fasta_path)
     return pd.DataFrame(process_fasta(fasta), columns=['header', 'sequence'])
 
 
@@ -162,29 +152,3 @@ def write_fasta_from_ncbi_query(pdb_codes, filepath, email, api_key):
     Entrez.api_key = api_key
     search_results, id_list = get_ncbi_search_results(pdb_codes)
     write_fasta_from_ncbi_fetch(search_results, id_list, filepath)
-
-
-def process_header(df):
-    header = df['header']
-    header = header.str.split('|')
-    df['pdb_code'] = header.str[1]
-    description = header.str[2]
-    df['chain'] = description.str[0]
-    df['description'] = description.str.split(', ').str[-1]
-    return df[['pdb_code', 'chain', 'description', 'sequence']]
-
-
-def organize_fasta_dataframe(df, column_names):
-    df = df[column_names]
-    return df.sort_values(['pdb_code', 'chain'], ignore_index=True)
-
-
-def sort_then_join(lst):
-    return ','.join(sorted(lst))
-
-
-# collapse rows that differ only by one column
-def collapse_rows(df, column_to_collapse):
-    columns = [n for n in df.columns if n != column_to_collapse]
-    df = df.groupby(columns)[column_to_collapse].apply(sort_then_join)
-    return df.reset_index()
