238080f18442142b2d43edcd5c4d22f219461458
stillahill <elliot.douglas.hill@gmail.com>
2021-11-17 11:27:03 -0500

    Add caching and refactor NCBI query functions

--- fastatools/fastatools.py
+++ fastatools/fastatools.py
@@ -37,7 +37,7 @@ class FastaDatasetMaker(DatasetMaker):
         return df.drop('header', axis=1)
 
     def sort_then_join(self, lst):
-        return ','.join(sorted(lst))
+        return ''.join(sorted(lst))
 
     # collapse rows that differ only by specified column
     def collapse_rows(self, df, column_to_collapse):
@@ -81,15 +81,20 @@ def make_query(pdb_code):
     return f'{pdb_code}[All Fields] AND pdb[filter]'
 
 
+def get_search_results(pdb_code):
+    query = make_query(pdb_code)
+    search_handle = Entrez.esearch(
+        db="protein", term=query, idtype="acc", usehistory="y", retmax=50)
+    search_results = Entrez.read(search_handle)
+    search_handle.close()
+    return search_results
+
+
 def make_fasta_id_list(pdb_codes):
     id_list = []
     n_codes = len(pdb_codes)
     for i, pdb_code in enumerate(pdb_codes):
-        query = make_query(pdb_code)
-        search_handle = Entrez.esearch(
-            db="protein", term=query, idtype="acc", usehistory="y", retmax=50)
-        search_results = Entrez.read(search_handle)
-        search_handle.close()
+        search_results = get_search_results(pdb_code)
         for _id in search_results['IdList']:
             id_list.append(_id)
         print(f'PDB code: {pdb_code}: {i+1} / {n_codes}')
@@ -99,15 +104,16 @@ def make_fasta_id_list(pdb_codes):
 
 def get_fasta_id_list(pdb_codes):
     print(f'Searching for {len(pdb_codes)} PDB entries')
-    id_list_filepath = Path('Data/Raw/fasta_id_list.txt')
+    id_list_path = Path('Data/Cache/')
+    id_list_path.mkdir(parents=True, exist_ok=True)
+    id_list_filepath = id_list_path / Path('fasta_id_list_cache.txt')
     if not id_list_filepath.is_file():
         id_list = make_fasta_id_list(pdb_codes)
         with open(id_list_filepath, 'w') as f:
             f.write(json.dumps(id_list))
-    else:
-        with open(id_list_filepath, 'r') as f:
-            id_list = json.loads(f.read())
-    return id_list
+        return id_list
+    with open(id_list_filepath, 'r') as f:
+        return json.loads(f.read())
 
 
 def get_ncbi_search_results(pdb_codes):
@@ -138,13 +144,28 @@ def fetch_fasta(search_results, id_count):
 def get_fasta_from_ncbi_query(pdb_codes, email, api_key):
     Entrez.email = email
     Entrez.api_key = api_key
-    search_results, id_list = get_ncbi_search_results(pdb_codes)
-    id_count = len(id_list)
-    print(f'Downloading {id_count} records')
-    return fetch_fasta(search_results, id_count)
+    list_path = Path('Data/Cache/')
+    list_path.mkdir(parents=True, exist_ok=True)
+    list_filepath = list_path / Path('fasta_cache.fa')
+    if not list_filepath.is_file():
+        search_results, id_list = get_ncbi_search_results(pdb_codes)
+        id_count = len(id_list)
+        print(f'Downloading {id_count} records')
+        fasta = fetch_fasta(search_results, id_count)
+        with open(list_filepath, 'w') as f:
+            f.write(json.dumps(fasta))
+        return fasta
+    with open(list_filepath, 'r') as f:
+        return json.loads(f.read())
 
 
 def write_fasta_from_ncbi_query(filepath, pdb_codes, email, api_key):
     data = get_fasta_from_ncbi_query(pdb_codes, email, api_key)
     with open(filepath, "w") as out_handle:
         out_handle.write(data)
+
+
+def save_data(df, path, filename, postfix):
+    path.mkdir(parents=True, exist_ok=True)
+    prefix = filename.split('.')[0]
+    df.to_csv(f'{path}/{prefix}_{postfix}.csv', index=False)
